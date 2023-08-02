9e88c356444d563ef5ed9252a14355b4b33de0cc
udarasan <udarassanjeewa@gmail.com>
2023-08-02 19:55:55 +0530

    eager fetching

---
+++ Eager-Feching/src/main/java/org/example/Main.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.example.entity.Customer;
+import org.example.utill.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class Main {
+    public static void main(String[] args) {
+//        Address address1=new Address();
+//        address1.setAid(1);
+//        address1.setName("Colombo");
+//
+//        Address address2=new Address();
+//        address2.setAid(2);
+//        address2.setName("Panadura");
+//
+//        Customer customer=new Customer();
+//        customer.setId(1);
+//        customer.setName("san");
+//        List<Address>addressList=new ArrayList<>();
+//        addressList.add(address1);
+//        addressList.add(address2);
+//        customer.setAddress(addressList);
+//        address1.setCustomer(customer);
+//        address2.setCustomer(customer);
+
+        Session session=
+                FactoryConfiguration.getInstance().getSession();
+        Transaction transaction=session.beginTransaction();
+
+//        session.persist(address1);
+//        session.persist(address2);
+//        session.persist(customer);
+
+        Customer customer=session.get(Customer.class,1);
+        System.out.println(customer.getId());
+        System.out.println(customer.getName());
+//        System.out.println(customer.getAddress());
+
+
+        transaction.commit();
+
+        session.close();
+    }
+}
